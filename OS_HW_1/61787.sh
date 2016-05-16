5799db1a5bd2f24135730e30038f6f0aa10691de
Martin Patsov <martinely95@yahoo.com>
2016-05-16 20:28:37 +0300

    added variables for the players and the board; printing the current state of the board

--- OS_HW_1/61787.sh
+++ OS_HW_1/61787.sh
@@ -24,6 +24,44 @@ read player2
 	shift
     done
 '
+#variables for "x" player
+x11=0
+x12=0
+x13=0
+x21=0
+x22=0
+x23=0
+v31=0
+v32=0
+x33=0
+
+#variables for "o" player
+o11=0
+o12=0
+o13=0
+o21=0
+o22=0
+o23=0
+v31=0
+v32=0
+o33=0
+
+#symbols
+v11=" "
+v12=" "
+v13=" "
+v21=" "
+v22=" "
+v23=" "
+v31=" "
+v32=" "
+v33=" "
+
+echo "Current board: "
+echo "|$v11|$v12|$v13|"
+echo "|$v21|$v22|$v23|"
+echo "|$v31|$v32|$v33|"
+
 end_of_game=false
 while [ true ]
 do
