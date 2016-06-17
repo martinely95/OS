5c8dd0e3f0430d2d431676588eaf367d138572a3
Martin Patsov <martinely95@yahoo.com>
2016-06-18 01:04:36 +0300

    Working pipe and output redirection

---
+++ OS_HW_2/tic-tac-toe-wrapper.c
@@ -0,0 +1,132 @@
+#include <stdio.h> //printf
+#include <unistd.h> //execl, execlp
+#include <sys/types.h> // fork, getpid, getppid
+#include <sys/wait.h> // wait
+#include <stdlib.h> // exit
+#include <fcntl.h>  // O_WRONLY|O_CREAT|O_TRUNC
+
+
+
+main(int argc, char* argv[]){
+    const int STDOUT = 1;
+    if (argc == 1) {
+        //debugging
+        // printf("%s", argv[1]);
+        int fd[2], nbytes; // file descriptors for the pipe, nbytes which are read from the pipe
+        int fd1;
+        char readbuffer[4096]; 
+        int i,j;
+        int status;
+        int returned_status;
+        int father;
+
+        if((father = fork()) == -1)
+        {
+                perror("fork");
+                exit(1);
+        }
+
+        if (father == 0)  // child
+        {
+            if ( execl("./tic-tac-toe.sh", "tic-tac-toe", NULL) == -1 ) {
+                exit(99);
+            }
+            exit(0);
+        }
+
+        wait(&status);
+        returned_status = status / 256;
+
+        //pipe(fd);
+        
+        if((father = fork()) == -1)
+        {
+                perror("fork");
+                exit(1);
+        }
+
+        if (father == 0)  // child
+        {
+
+            if( ( fd1 = open("temp", O_WRONLY|O_CREAT|O_TRUNC, 0600 ) ) == -1 ){
+                    write(2, "Something went wrong!\n", sizeof("Something went wrong!\n")-1);
+                    exit(99);
+            }
+            else {
+                     /* Child process closes up output side of pipe */
+                    // close(1);
+                    // dup(fd1);
+                    dup2(fd1, 1);
+
+                    /* Send "string" through the input side of pipe */
+                    //write(fd[0], string, (strlen(string)+1));
+                    execlp("ls", "ls", "./.tic-tac-toe", "-t", NULL);
+            }          
+        }
+        //todo: da podam na funkciqta temp faila vmesto pipe
+        
+        wait(&status);
+        returned_status = status / 256;
+
+        // if (fcntl(fd1, F_GETFD)) { // if fd1 is not closed
+        //     close(fd1);
+        // }
+        pipe(fd);
+        if((father = fork()) == -1)
+        {
+                perror("fork");
+                exit(1);
+        }
+
+        if (father == 0)  // child
+        {
+
+            if( ( fd1 = open("temp", O_RDONLY, 0400 ) ) == -1 ){
+                    write(2, "Something went wrong!\n", sizeof("Something went wrong!\n")-1);
+                    exit(99);
+            }
+            else {
+                    close(fd[0]);
+                    // dup(fd1);
+                    dup2(fd[1], 1);
+
+                    /* Send "string" through the input side of pipe */
+                    //write(fd[0], string, (strlen(string)+1));
+                    execlp("head", "head", "-n", "1", "temp", NULL);
+            }          
+        }
+
+        if (father > 0) {
+                /* Parent process closes up output side of pipe */
+                close(fd[1]);
+
+                /* Read in a string from the pipe */
+                nbytes = read(fd[0], readbuffer, sizeof(readbuffer));
+                printf("Received string: %s", readbuffer);
+        }
+        
+        wait(&status);
+        returned_status = status / 256;
+
+        
+
+
+        if ( father > 0 ){
+            //father body
+            // status returns the PID 
+            
+            
+            printf("Status of execution of tic-tac-toe: %d\n", returned_status);
+            if (returned_status == 99) {
+                write(STDOUT, "Unsuccesful opening of file.\n", sizeof("Unsuccesful opening of file.\n")-1);
+            } 
+            else if (returned_status == 0) {
+                write(STDOUT, "Game session ended.\n", sizeof("Game session ended.\n")-1);
+            }
+        } 
+        else {
+            //child body
+            
+        }
+    }
+}
---
+++ OS_HW_2/tic-tac-toe.sh
@@ -0,0 +1,224 @@
+#!/bin/bash
+
+validate_move() {
+	if [ "$1" -le 2 -a "$2" -le 2 -a "$1" -ge 0 -a "$2" -ge 0 ] 2>/dev/null #catch error
+	then
+		return 1
+	fi
+	return 0
+}
+
+timestamp() {
+  date +"%s"
+}
+
+echo "Име на играч 1 - O:"
+read player1
+echo "Име на играч 2 - X:"
+read player2
+echo ""
+
+mkdir .tic-tac-toe 2>/dev/null
+
+ts=$(timestamp)
+touch ./.tic-tac-toe/$player1-$player2-$ts.log
+
+#remove:
+exit 0
+
+playing='y'
+while [ "$playing" == 'y' ]
+do
+
+	#variables for "x" player
+	x00=0
+	x01=0
+	x02=0
+	x10=0
+	x11=0
+	x12=0
+	x20=0
+	x21=0
+	x22=0
+
+	#variables for "o" player
+	o00=0
+	o01=0
+	o02=0
+	o10=0
+	o11=0
+	o12=0
+	o20=0
+	o21=0
+	o22=0
+
+	#symbols
+	v00=" "
+	v01=" "
+	v02=" "
+	v10=" "
+	v11=" "
+	v12=" "
+	v20=" "
+	v21=" "
+	v22=" "
+
+	while [ true ]
+	do
+
+		
+
+		echo "$player1 - изберете поле (ред, колона): "
+		valid=0
+		while [ "$valid" -eq 0 ]
+		do
+
+			read player1_move
+			row=$(expr substr "$player1_move" 2 1)
+			col=$(expr substr "$player1_move" 5 1)
+
+			validate_move "$row" "$col"
+			valid=$?
+
+			if [ "$valid" -ne 1 ] 
+			then
+				echo "Невалидна позиция. Моля въведете отново: "
+			else
+				val="v$row$col"
+				tmp=${!val}
+				if ! [ "$tmp" == " " ] 
+				then
+					echo "Невалидна позиция. На позицията вече има $tmp. Моля въведете отново: "
+					valid=0
+				fi
+			fi				
+		done
+
+		#echo "$player1 - o: $player1_move" >> ./.tic-tac-toe/$player1-$player2-$ts.log
+		echo "$player1 - $player1_move" >> ./.tic-tac-toe/$player1-$player2-$ts.log
+
+		#echo "$row $col"
+		var="o$row$col"
+		eval $var="1"
+		val="v$row$col"
+		eval $val="o"
+		#echo "${!var}"
+
+		echo ""
+		#cho "Текущо състояние на играта: "
+		echo "|$v00|$v01|$v02|"
+		echo "|$v10|$v11|$v12|"
+		echo "|$v20|$v21|$v22|"
+		echo ""
+
+		orows=$(( ($o00 & $o01 & $o02) | ( $o10 & $o11 & $o12 ) | ( $o20 & $o21 & $o22 ) ))
+		ocols=$(( ($o00 & $o10 & $o20) | ( $o01 & $o11 & $o21 ) | ( $o02 & $o12 & $o22 ) ))
+		odiags=$(( ($o00 & $o11 & $o22) | ( $o02 & $o11 & $o20 ) ))
+		owins=$(( $orows | $ocols | $odiags ))
+		#echo "$ocols $orows $odiags $owins"
+
+		remi=$(( $x00 + $x01 + $x02 + $x10 + $x11 + $x12 + $x20 + $x21 + $x22 + $o00 + $o01 + $o02 + $o10 + $o11 + $o12 + $o20 + $o21 + $o22 ))
+		#echo $remi
+
+		if [ "$owins" == "1" ]
+		then
+			echo "Печели $player1"
+			echo ""
+
+			echo "Печели $player1" >> ./.tic-tac-toe/$player1-$player2-$ts.log
+			echo "" >> ./.tic-tac-toe/$player1-$player2-$ts.log
+			
+			break
+		fi
+
+		if [ "$remi" == "9" ]
+		then
+			echo "Играта е реми"
+			echo ""
+
+			echo "Играта е реми" >> ./.tic-tac-toe/$player1-$player2-$ts.log
+			echo "" >> ./.tic-tac-toe/$player1-$player2-$ts.log
+
+			break
+		fi
+
+
+		echo "$player2 - изберете поле (ред, колона): "
+		valid=0
+		while [ "$valid" -eq 0 ]
+		do
+
+			read player2_move
+			row=$(expr substr "$player2_move" 2 1)
+			col=$(expr substr "$player2_move" 5 1)
+
+			validate_move "$row" "$col"
+			valid=$?
+
+			if [ "$valid" -ne 1 ] 
+			then
+				echo "Невалидна позиция. Моля въведете отново: "
+			else
+				val="v$row$col"
+				tmp=${!val}
+				if ! [ "$tmp" == " " ] 
+				then
+					echo "Невалидна позиция. На позицията вече има $tmp. Моля въведете отново: "
+					valid=0
+				fi
+			fi
+		done
+
+		#echo "$player2 - x: $player2_move" >> ./.tic-tac-toe/$player1-$player2-$ts.log
+		echo "$player2 - $player2_move" >> ./.tic-tac-toe/$player1-$player2-$ts.log
+
+		var="x$row$col"
+		eval $var="1"
+		val="v$row$col"
+		eval $val="x"
+
+		echo ""
+		#echo "Текущо състояние на играта: "
+		echo "|$v00|$v01|$v02|"
+		echo "|$v10|$v11|$v12|"
+		echo "|$v20|$v21|$v22|"
+		echo ""
+
+		xrows=$(( ($x00 & $x01 & $x02) | ( $x10 & $x11 & $x12 ) | ( $x20 & $x21 & $x22 ) ))
+		xcols=$(( ($x00 & $x10 & $x20) | ( $x01 & $x11 & $x21 ) | ( $x02 & $x12 & $x22 ) ))
+		xdiags=$(( ($x00 & $x11 & $x22) | ( $x02 & $x11 & $x20 ) ))
+		xwins=$(( $xrows | $xcols | $xdiags ))
+		#echo "$xcols $xrows $xdiags $xwins"
+		remi=$(( $x00 + $x01 + $x02 + $x10 + $x11 + $x12 + $x20 + $x21 + $x22 + $o00 + $o01 + $o02 + $o10 + $o11 + $o12 + $o20 + $o21 + $o22 ))
+		#echo $remi
+
+		if [ "$xwins" == "1" ]
+		then
+			echo "Печели $player2"
+			echo ""
+			
+			echo "Печели $player2" >> ./.tic-tac-toe/$player1-$player2-$ts.log
+			echo "" >> ./.tic-tac-toe/$player1-$player2-$ts.log
+
+			break
+		fi
+
+		if [ "$remi" == "9" ]
+		then			
+			echo "Играта е реми"
+			echo ""
+
+			echo "Играта е реми" >> ./.tic-tac-toe/$player1-$player2-$ts.log
+			echo "" >> ./.tic-tac-toe/$player1-$player2-$ts.log
+
+			break
+		fi
+
+	done
+
+	echo 'Повторна игра ? (y/n):'
+	read playing
+
+	echo ""
+
+done
