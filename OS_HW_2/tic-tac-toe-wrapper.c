2fd05fa0013c3dd415738e1f05fe78cdf552bfbd
Martin Patsov <martinely95@yahoo.com>
2016-06-21 04:15:39 +0300

    Adding summary to log file

--- OS_HW_2/tic-tac-toe-wrapper.c
+++ OS_HW_2/tic-tac-toe-wrapper.c
@@ -174,8 +174,18 @@ main(int argc, char* argv[]){
         /* Read in a string from the pipe */
         readbuffer[0]='\0';
         nbytes = read(fd2[STDIN], readbuffer, sizeof(readbuffer));
-        write(STDOUT, "Games played: \n", sizeof("Games played: \n")-1);
-        write(STDOUT, readbuffer, sizeof(nbytes)-1);
+        //readbuffer[nbytes]='\0';
+
+        char *n4 = strrchr(readbuffer, '\n');
+        if (n4)
+            *n4 = 0;
+
+        char * allGamesCount = malloc(nbytes+1); 
+        strcpy(allGamesCount, readbuffer);
+
+        // readbuffer[0] = nbytes + '0';
+        // readbuffer[1] = '\0';
+
         
         wait(&status);
         returned_status = status / 256;
@@ -241,8 +251,103 @@ main(int argc, char* argv[]){
         /* Read in a string from the pipe */
         readbuffer[0]='\0';
         nbytes = read(fd2[STDIN], readbuffer, sizeof(readbuffer));
-        write(STDOUT, "Remi: \n", sizeof("Remi: \n")-1);
-        write(STDOUT, readbuffer, sizeof(nbytes)-1);
+        // write(STDOUT, "Remi: \n", sizeof("Remi: \n")-1);
+        // write(STDOUT, readbuffer, nbytes);
+
+        char *n3 = strrchr(readbuffer, '\n');
+        if (n3)
+            *n3 = 0;
+
+        char * remiCount = malloc(nbytes+1); 
+        strcpy(remiCount, readbuffer);
+        //write(STDOUT, remiCount, nbytes);
+        //strcat(remiCount, ime1);
+
+
+
+        
+        wait(&status);
+        returned_status = status / 256;
+
+        close(fd2[STDIN]); 
+
+        // masovo kopirane na kod2
+        // do not do this at home
+        // vzemame obsh broj igri
+        // int fd[2];//,  // file descriptors for the pipe, nbytes which are read from the pipe
+        pipe(fd);
+
+        // grep na pobedi i remita
+        if((father = fork()) == -1)
+        {
+            perror("fork");
+            exit(1);
+        }
+
+        if (father == 0)  // child
+        {
+            chdir("./.tic-tac-toe");
+            //execlp("pwd", "pwd", NULL);  
+            close(fd[STDIN]); //close the side of the pipe that will not be used
+            dup2(fd[STDOUT], STDOUT);
+            //write(STDOUT, "\n", sizeof("\n"));
+            char * grepString = malloc(strlen(ime1)+strlen("Печели ")+1); 
+            strcpy(grepString, "Печели ");
+            strcat(grepString, ime1);
+
+
+            // execlp("echo", "echo", grepString, NULL);  
+            execlp("grep", "grep", grepString, logFileName, NULL);       
+        }
+
+        // wait(&status);
+        // returned_status = status / 256;
+
+        close(fd[STDOUT]);
+        // wc na redovete varnati ot grep
+
+        // int fd2[2]; // this is where the fun starts
+        pipe(fd2);
+        if((father = fork()) == -1)
+        {
+            perror("fork");
+            exit(1);
+        }
+
+        // int nbytes;
+        // char readbuffer[4096];
+
+        if (father == 0)  // child
+        {
+            close(fd2[STDIN]); 
+
+            dup2(fd[STDIN], STDIN);
+            dup2(fd2[STDOUT], STDOUT);
+
+            execlp("wc", "wc", "-l", NULL);       
+        }
+
+        // wait(&status);
+        // returned_status = status / 256;
+
+        close(fd2[STDOUT]);
+        close(fd[STDIN]); 
+
+        /* Read in a string from the pipe */
+        readbuffer[0]='\0';
+        nbytes = read(fd2[STDIN], readbuffer, sizeof(readbuffer));
+        // write(STDOUT, "Pobedi na ", sizeof("Pobedi na ")-1);
+        // write(STDOUT, ime1, sizeof(ime1)-1);
+        // write(STDOUT, ":\n", sizeof(":\n")-1);
+        // write(STDOUT, readbuffer, nbytes);
+
+        char *n1 = strrchr(readbuffer, '\n');
+        if (n1)
+            *n1 = 0;
+        char * win1 = malloc(nbytes+1); 
+        strcpy(win1, readbuffer);
+        // write(STDOUT, win1, nbytes);
+        //strcat(remiCount, ime1);
         
         wait(&status);
         returned_status = status / 256;
@@ -250,11 +355,130 @@ main(int argc, char* argv[]){
         close(fd2[STDIN]); 
 
 
+        // masovo kopirane na kod3
+        // i daje raboti
+        // do not do this at home
+        // vzemame obsh broj igri
+        // int fd[2];//,  // file descriptors for the pipe, nbytes which are read from the pipe
+        pipe(fd);
+
+        // grep na pobedi i remita
+        if((father = fork()) == -1)
+        {
+            perror("fork");
+            exit(1);
+        }
+
+        if (father == 0)  // child
+        {
+            chdir("./.tic-tac-toe");
+            //execlp("pwd", "pwd", NULL);  
+            close(fd[STDIN]); //close the side of the pipe that will not be used
+            dup2(fd[STDOUT], STDOUT);
+            //write(STDOUT, "\n", sizeof("\n"));
+            char * grepString = malloc(strlen(ime1)+strlen("Печели ")+1); 
+            strcpy(grepString, "Печели ");
+            strcat(grepString, ime2);
 
 
+            // execlp("echo", "echo", grepString, NULL);  
+            execlp("grep", "grep", grepString, logFileName, NULL);       
+        }
 
+        // wait(&status);
+        // returned_status = status / 256;
 
+        close(fd[STDOUT]);
+        // wc na redovete varnati ot grep
+
+        // int fd2[2]; // this is where the fun starts
+        pipe(fd2);
+        if((father = fork()) == -1)
+        {
+            perror("fork");
+            exit(1);
+        }
+
+        // int nbytes;
+        // char readbuffer[4096];
+
+        if (father == 0)  // child
+        {
+            close(fd2[STDIN]); 
+
+            dup2(fd[STDIN], STDIN);
+            dup2(fd2[STDOUT], STDOUT);
+
+            execlp("wc", "wc", "-l", NULL);       
+        }
+
+        // wait(&status);
+        // returned_status = status / 256;
+
+        close(fd2[STDOUT]);
+        close(fd[STDIN]); 
+
+        /* Read in a string from the pipe */
+        readbuffer[0]='\0';
+        nbytes = read(fd2[STDIN], readbuffer, sizeof(readbuffer));
+        // write(STDOUT, "Pobedi na ", sizeof("Pobedi na ")-1);
+        // write(STDOUT, ime2, sizeof(ime2)-1);
+        // write(STDOUT, ":\n", sizeof(":\n")-1);
+        // write(STDOUT, readbuffer, nbytes);
+
+        char *n2 = strrchr(readbuffer, '\n');
+        if (n2)
+            *n2 = 0;
+        char * win2 = malloc(nbytes+1); 
+        strcpy(win2, readbuffer);
+        // write(STDOUT, win2, nbytes);
+        //strcat(remiCount, ime1);
         
+        wait(&status);
+        returned_status = status / 256;
+
+        close(fd2[STDIN]); 
+
+
+        char * logfileFullPath = malloc(strlen("./.tic-tac-toe/")+strlen(logFileName)+1); 
+        strcpy(logfileFullPath, "./.tic-tac-toe/");
+        strcat(logfileFullPath, logFileName);
+
+        int stdout_copy = dup(STDOUT);
+
+        if( ( fd1 = open(logfileFullPath, O_WRONLY|O_APPEND, 0200 ) ) == -1 ){
+            write(2, "Something went wrong!\n", sizeof("Something went wrong!\n")-1);
+            exit(99);
+        }
+        else {                
+            dup2(fd1, STDOUT);
+            //execlp("ls", "ls", "./.tic-tac-toe", "-t", NULL);
+        }      
+
+        write(STDOUT, "\n", strlen("\n"));
+        write(STDOUT, "Общ брой игри: ", strlen("Общ брой игри: "));
+        write(STDOUT, allGamesCount, strlen(allGamesCount));
+        write(STDOUT, "\n", strlen("\n"));
+
+        write(STDOUT, ime1, strlen(ime1));
+        write(STDOUT, " ", strlen(" "));
+        write(STDOUT, win1, strlen(win1));      
+        write(STDOUT, " ", strlen(" "));
+        write(STDOUT, win2, strlen(win2));      
+        write(STDOUT, " ", strlen(" "));
+        write(STDOUT, remiCount, strlen(remiCount)); 
+        write(STDOUT, "\n", strlen("\n"));
+
+        write(STDOUT, ime2, strlen(ime2));
+        write(STDOUT, " ", strlen(" "));
+        write(STDOUT, win2, strlen(win2));      
+        write(STDOUT, " ", strlen(" "));
+        write(STDOUT, win1, strlen(win1));      
+        write(STDOUT, " ", strlen(" "));
+        write(STDOUT, remiCount, strlen(remiCount)); 
+        write(STDOUT, "\n", strlen("\n"));
+
+        dup2(stdout_copy, STDOUT);        
 
 
         if ( father > 0 ){
@@ -262,16 +486,20 @@ main(int argc, char* argv[]){
             // status returns the PID 
             
             
-            write(STDOUT, "Status of execution of tic-tac-toe: \n", sizeof("Status of execution of tic-tac-toe: \n"));
-            readbuffer[0] = returned_status + '0';
-            readbuffer[1] = '\0';
-            write(STDOUT, readbuffer, sizeof(returned_status)-1);
+            // write(STDOUT, "Status of execution of tic-tac-toe: ", sizeof("Status of execution of tic-tac-toe: ")-1);
+            // readbuffer[0] = returned_status + '0';
+            // readbuffer[1] = '\0';
+            // write(STDOUT, readbuffer, sizeof(returned_status)-1);
+
+            // write(STDOUT, "\n", sizeof("\n")-1);
 
             if (returned_status == 99) {
-                write(STDOUT, "Unsuccesful opening of file.\n", sizeof("Unsuccesful opening of file.\n")-1);
+                // write(STDOUT, "Unsuccesful opening of file.\n", sizeof("Unsuccesful opening of file.\n")-1);
+                exit(99);
             } 
             else if (returned_status == 0) {
-                write(STDOUT, "Game session ended.\n", sizeof("Game session ended.\n")-1);
+                // write(STDOUT, "Game session ended.\n", sizeof("Game session ended.\n")-1);
+                exit(0);
             }
         } 
         else {
